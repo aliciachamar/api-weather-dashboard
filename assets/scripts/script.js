2be0593fc0fa8061e37902142a4a7eaa370ab5cf
aliciachamar <aliciachamar@gmail.com>
2021-05-25 15:07:02 -0600

    added icons

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,14 +7,15 @@ const $tempSpan = $("#tempSpan");
 const $windSpan = $("#windSpan");
 const $humiditySpan = $("#humiditySpan");
 const $indexSpan = $("#indexSpan");
+const $forecast = $(".forecast");
 
-$searchBtn.on("click", function () {
-    runSearch(event);
-});
+$searchBtn.on("click", runSearch);
 
 function runSearch(event) {
     event.preventDefault();
-
+    for (let i = 0; i < $forecast.length; i++) {
+        $forecast[i].text("");
+    }
     city = $searchInput.val();
     const firstRequestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=56ad829811fedde89e78e8505c048c6f";
     fetch(firstRequestURL)
@@ -27,12 +28,13 @@ function runSearch(event) {
                     .then(function (response) {
                         response.json().then(function (data) {
                             console.log(data);
+                            storeWeather(data, city);
                             displayWeather(data, city);
                         })
                     })
             })
         }) 
-    addHistory(city);
+        addHistory(city);
     $searchInput.val("");
 }
 
@@ -43,11 +45,49 @@ function displayWeather(data, city) {
     $humiditySpan.text(data.current.humidity);
     $indexSpan.text(data.current.uvi);
     $indexSpan.attr("class", "");
-    if (0 < data.current.uvi < 2) {
+    if (data.current.uvi > 0 && data.current.uvi < 2) {
         $indexSpan.addClass("favorable");
-    } else if (2 < data.current.uvi < 5) {
+    } else if (data.current.uvi > 2 && data.current.uvi < 5) {
         $indexSpan.addClass("moderate");
     } else {
         $indexSpan.addClass("severe");
     }
+    for (let i = 0; i < $forecast.length; i++) {
+        let $date = document.createElement("h3");
+        $date.textContent = moment().add((i+1), "days").format("MM/DD/YYYY");
+        let $icon = document.createElement("img");
+        if (data.daily[i].weather[0].icon === "01d") {
+            $icon.setAttribute("src", "http://openweathermap.org/img/wn/01d@2x.png");
+        } else if (data.daily[i].weather[0].icon === "02d") {
+            $icon.setAttribute("src", "http://openweathermap.org/img/wn/02d@2x.png");
+        } else if (data.daily[i].weather[0].icon === "03d") {
+            $icon.setAttribute("src", "http://openweathermap.org/img/wn/03d@2x.png");
+        } else if (data.daily[i].weather[0].icon === "04d") {
+            $icon.setAttribute("src", "http://openweathermap.org/img/wn/04d@2x.png");
+        }
+        let $temp = document.createElement("p");
+        $temp.textContent = "Temp: " + data.daily[i].temp.day + "° F";
+        let $wind = document.createElement("p");
+        $wind.textContent = "Wind: " + data.daily[i].wind_speed + " mph";
+        let $humidity = document.createElement("p");
+        $humidity.textContent = "Humidity: " + data.daily[i].humidity + "%";
+        $forecast[i].append($date, $icon, $temp, $wind, $humidity);
+    }
+}
+
+function addHistory(city) {
+    let $oldCity = $("<button class='btn'>" + city + "</button>");
+    $searchHistory.append($oldCity);
+    $oldCity.on("click", function() {
+        loadStoredData(city);
+    });
+}
+
+function storeWeather(data, city) {
+    localStorage.setItem(city, JSON.stringify(data));
+}
+
+function loadStoredData(city) {
+    let data = JSON.parse(localStorage.getItem(city));
+    displayWeather(data);
 }
